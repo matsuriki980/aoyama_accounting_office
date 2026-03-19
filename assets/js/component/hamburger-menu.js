a62d0d4c6f46957d4f70975b1ff496af4ebfcfea
matsuyamarikiya <maturiki17@ymail.ne.jp>
2026-03-19 21:47:05 +0900

    header 構築
    ・ヘッダーナビゲーション
    ・ハンバーガーメニュー

---
+++ assets/js/component/hamburger-menu.js
@@ -0,0 +1,17 @@
+/*
+  ハンバーガーメニュー
+*/
+
+export const initializeHamburgerMenu = () => {
+  const menu = document.querySelector(".js-menu");
+  const openBtn = document.querySelector(".js-menu__button_open");
+  const cloceBtn = document.querySelector(".js-menu__button_close");
+  // const closeBtn = document.querySelector(".js-header-menu-close-button");
+
+ // if (!menu || !openBtn || !cloceBtn) return;
+
+  menu.showModal();
+
+  // btn.addEventListener("click", () => {
+  // });
+};
