814f79d408e57e5d8d63fca0737e1f61639fdc84
matsuyamarikiya <maturiki17@ymail.ne.jp>
2026-04-16 12:44:53 +0900

    feat: ヘッダー背景のスクロールアニメーションを実装

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
+import { initializeHeaderBg } from "./component/header-bg.js";
 import { initializeDrawerMenu } from "./component/drawer-menu.js";
 
 window.addEventListener("load", () => {
   // 全ての初期化を実行
+  initializeHeaderBg();
   initializeDrawerMenu();
 });
