afb9e4bd4244941522e123fed9ec4ca41dcd2152
matsuyamarikiya <maturiki17@ymail.ne.jp>
2026-04-17 18:37:13 +0900

    fix: インポートファイル名の変更

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 import { initializeHeaderBg } from "./component/header-bg.js";
-import { initializeDrawerMenu } from "./component/drawer-menu.js";
+import { initializeDrawerMenu } from "./component/drawer.js";
 
 window.addEventListener("load", () => {
   // 全ての初期化を実行
