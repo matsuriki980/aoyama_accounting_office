9c72718b9c6d1bd5f1c71369b7b642e189fd7523
matsuyamarikiya <maturiki17@ymail.ne.jp>
2026-04-20 15:33:03 +0900

    feat: topページのスライダー用のjsファイルを追加

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,12 @@
 import { initializeHeaderBg } from "./component/header-bg.js";
 import { initializeDrawerMenu } from "./component/drawer.js";
+import { initializeServiceSlider } from "./slider/top-service-slider.js";
+import { initializeTopVoiceSlider } from "./slider/top-voice-slider.js";
 
 window.addEventListener("load", () => {
   // 全ての初期化を実行
   initializeHeaderBg();
   initializeDrawerMenu();
+  initializeServiceSlider();
+  initializeTopVoiceSlider();
 });
